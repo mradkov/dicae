f4def74b4c6db2cfcb34644d0be3b5672eff67de
Milen Radkov <milenradkov@me.com>
2019-09-15 12:22:45 +0200

    use local compiler aeproject

--- test/dicaeTest.js
+++ test/dicaeTest.js
@@ -14,49 +14,65 @@
  *  OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR
  *  PERFORMANCE OF THIS SOFTWARE.
  */
-const path = require('path');
-const chai = require('chai');
-let chaiAsPromised = require("chai-as-promised");
-chai.use(chaiAsPromised);
-const assert = chai.assert;
+import {encodeBase58Check, encodeUnsigned, hash} from "@aeternity/aepp-sdk/es/utils/crypto";
 
-const AeSDK = require('@aeternity/aepp-sdk');
-const Universal = AeSDK.Universal;
-const config = require("./constants/config.json")
-const utils = require('./utils/utils');
+const Universal = require('@aeternity/aepp-sdk').Universal;
+const Bytes = require('@aeternity/aepp-sdk/es/utils/bytes');
+const Crypto = require('@aeternity/aepp-sdk').Crypto;
+const blake2b = require('blake2b');
+const BN = require('bn.js');
 
-const CONTRACT_FILE_PATH = "./../contracts/Dicae.aes";
-const getClient = utils.getClient;
+const Deployer = require('aeproject-lib').Deployer;
+const migrationSource = utils.readFileRelative('./contracts/Dicae.aes', 'utf-8');
 
-const contentOfContract = utils.readFileRelative(path.resolve(__dirname, CONTRACT_FILE_PATH), config.filesEncoding);
 
-describe('Dicae Contract', async () => {
+const config = {
+    host: 'http://localhost:3001/',
+    internalHost: 'http://localhost:3001/internal/',
+    compilerUrl: 'http://localhost:3080'
+};
 
-  let contractInstance;
+describe('Dicae Contract', () => {
 
-  beforeEach(async () => {
-      let client = await getClient(Universal, config, config.ownerKeyPair);
+    let ownerKeypair, ownerClient, secondKeypair, secondClient, thirdKeypair;
+    let contractInstance;
 
-      contractInstance = await client.getContractInstance(contentOfContract);
-      await contractInstance.deploy([]);
-  });
+    before(async () => {
+        ownerKeypair = wallets[0];
+        secondKeypair = wallets[1];
+        thirdKeypair = wallets[2];
+        ownerClient = await Universal({
+            url: config.host,
+            internalUrl: config.internalHost,
+            keypair: ownerKeypair,
+            nativeMode: true,
+            networkId: 'ae_devnet',
+            compilerUrl: config.compilerUrl
+        });
+    });
 
-  it('Deploying Dicae Contract', async () => {
-    assert(contractInstance, 'Could not deploy the Dicae Smart Contract'); // Check it is deployed
-  })
+    const hashTopic = topic => blake2b(32).update(Buffer.from(topic)).digest('hex');
+    const topicHashFromResult = result => Bytes.toBytes(result.result.log[0].topics[0], true).toString('hex');
 
-  it('should place bet correctly', async () => {
-    let number = 5;
+    const eventArgument = (result, index) => result.result.log[0].topics[index + 1];
+    const encodeEventAddress = (result, index, prefix) => `${prefix}${Crypto.encodeBase58Check(new BN(result.result.log[0].topics[index + 1]).toBuffer('be', 32))}`;
 
-    const result = await contractInstance.methods.bet(number);
-    assert.ok(result.decodedResult, "It doesnt bet properly");
-  })
-
-  it('should return random number between 1 and 6', async () => {
-    let possibleOutputs = [0,1,2,3,4,5];
-
-    const result = await contractInstance.methods.get_random();
-    assert.equal(possibleOutputs.indexOf(result.decodedResult) > -1, true, "It doesnt return random number between 1 and 6");
-  })
+    it('Deploying Migration', async () => {
+        contractInstance = await ownerClient.getContractInstance(migrationSource);
+        const init = await contractInstance.methods.init();
+        assert.equal(init.result.returnType, 'ok');
+    });
+    
+    it('should place bet correctly', async () => {
+        let number = 5;
+        const result = await contractInstance.methods.bet(number);
+        assert.ok(result.decodedResult, "It doesnt bet properly");
+    })
+    
+    it('should return random number between 1 and 6', async () => {
+        let possibleOutputs = [0,1,2,3,4,5];
+        const result = await contractInstance.methods.get_random();
+        assert.equal(possibleOutputs.indexOf(result.decodedResult) > -1, true, "It doesnt return random number between 1 and 6");
+    })
 
 })
