84920abd5171f6f74efab64c93445f92ba35b8ae
Milen Radkov <milenradkov@me.com>
2019-09-15 11:51:54 +0200

    fix out_of_gas issue

--- test/dicaeTest.js
+++ test/dicaeTest.js
@@ -14,58 +14,32 @@
  *  OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR
  *  PERFORMANCE OF THIS SOFTWARE.
  */
-const Ae = require('@aeternity/aepp-sdk').Universal;
-const Crypto = require('@aeternity/aepp-sdk').Crypto;
+const path = require('path');
+const chai = require('chai');
+let chaiAsPromised = require("chai-as-promised");
+chai.use(chaiAsPromised);
+const assert = chai.assert;
 
-const config = {
-  host: "http://localhost:3001/",
-  internalHost: "http://localhost:3001/internal/",
-  contractSourcePath: "./contracts/Dicae.aes",
-  gas: 200000,
-  ttl: 55
-}
+const AeSDK = require('@aeternity/aepp-sdk');
+const Universal = AeSDK.Universal;
+const config = require("./constants/config.json")
+const utils = require('./utils/utils');
 
-describe('Dicae Contract', () => {
+const CONTRACT_FILE_PATH = "./../contracts/Dicae.aes";
+const getClient = utils.getClient;
 
-  let owner;
-  let nonOwner;
-  let contractSource;
-  let compiledContract;
+const contentOfContract = utils.readFileRelative(path.resolve(__dirname, CONTRACT_FILE_PATH), config.filesEncoding);
+
+describe('Dicae Contract', async () => {
+
   let contractInstance;
 
-  before(async () => {
-    owner = await Ae({
-      url: config.host,
-      internalUrl: config.internalHost,
-      keypair: {
-        publicKey: "ak_2mwRmUeYmfuW93ti9HMSUJzCk1EYcQEfikVSzgo6k2VghsWhgU",
-        secretKey: "bb9f0b01c8c9553cfbaf7ef81a50f977b1326801ebf7294d1c2cbccdedf27476e9bbf604e611b5460a3b3999e9771b6f60417d73ce7c5519e12f7e127a1225ca"
-      },
-      nativeMode: true,
-      networkId: 'ae_devnet'
-    });
+  beforeEach(async () => {
+      let client = await getClient(Universal, config, config.ownerKeyPair);
 
-    nonOwner = await Ae({
-      url: config.host,
-      internalUrl: config.internalHost,
-      keypair: wallets[1],
-      nativeMode: true,
-      networkId: 'ae_devnet'
-    });
-
-    // Read the source file
-    contractSource = utils.readFileRelative(config.contractSourcePath, "utf-8");
-    compiledContract = await owner.contractCompile(contractSource, { // Compile it
-      gas: config.gas
-    })
-
-    contractInstance = await compiledContract.deploy({ // Deploy it
-      options: {
-        ttl: config.ttl,
-      },
-      abi: "sophia"
-    });
-  })
+      contractInstance = await client.getContractInstance(contentOfContract);
+      await contractInstance.deploy([]);
+  });
 
   it('Deploying Dicae Contract', async () => {
     assert(contractInstance, 'Could not deploy the Dicae Smart Contract'); // Check it is deployed
@@ -73,33 +47,16 @@ describe('Dicae Contract', () => {
 
   it('should place bet correctly', async () => {
     let number = 5;
-    
-    const contractCall = await owner.contractCall(compiledContract.bytecode, 'sophia', contractInstance.address, "bet", {
-      args: `(${number})`,
-      options: {
-        amount: 1000000000000000000,
-        ttl: config.ttl
-      },
-      abi: "sophia"
-    })
 
-    const contractCallResult = await contractCall.decode('bool');
-    assert.equal(contractCallResult.value, true, "It doesnt bet properly");
+    const result = await contractInstance.methods.bet(number);
+    assert.ok(result.decodedResult, "It doesnt bet properly");
   })
 
   it('should return random number between 1 and 6', async () => {
     let possibleOutputs = [0,1,2,3,4,5];
 
-    const contractCall = await owner.contractCall(compiledContract.bytecode, 'sophia', contractInstance.address, "get_random", {
-      args: `()`,
-      options: {
-        ttl: config.ttl
-      },
-      abi: "sophia"
-    })
-
-    const contractCallResult = await contractCall.decode('int');
-    assert.equal(possibleOutputs.indexOf(contractCallResult.value) > -1, true, "It doesnt return random number between 1 and 6");
+    const result = await contractInstance.methods.get_random();
+    assert.equal(possibleOutputs.indexOf(result.decodedResult) > -1, true, "It doesnt return random number between 1 and 6");
   })
 
 })
